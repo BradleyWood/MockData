0d45bab0fa4f56d22b6244bf48379240f415c905
BradleyWood <bradley.wood@uoit.net>
2018-04-07 19:11:46 -0400

    make fields streamable

--- src/main/java/org/mockdata/model/DataField.java
+++ src/main/java/org/mockdata/model/DataField.java
@@ -2,8 +2,42 @@ package org.mockdata.model;
 
 import org.jetbrains.annotations.NotNull;
 
-public abstract class DataField<T> implements Verifiable<T> {
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public abstract class DataField<T> implements Verifiable<T>, Iterable<T> {
 
     @NotNull
     public abstract T generate();
+
+    public final Stream<T> stream() {
+        return StreamSupport.stream(spliterator(), false);
+    }
+
+    @Override
+    public Spliterator<T> spliterator() {
+        return Spliterators.spliteratorUnknownSize(iterator(), 0);
+    }
+
+    @NotNull
+    @Override
+    public Iterator<T> iterator() {
+        return new FieldItr();
+    }
+
+    private class FieldItr implements Iterator<T> {
+
+        @Override
+        public boolean hasNext() {
+            return true;
+        }
+
+        @Override
+        public T next() {
+            return generate();
+        }
+    }
 }
