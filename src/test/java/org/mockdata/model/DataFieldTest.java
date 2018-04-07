50a9114d2aa8876742d55949bf8621e1974660c5
BradleyWood <bradley.wood@uoit.net>
2018-04-07 19:14:53 -0400

    test streamed field generation

---
+++ src/test/java/org/mockdata/model/DataFieldTest.java
@@ -0,0 +1,15 @@
+package org.mockdata.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DataFieldTest {
+
+    @Test
+    public void testStream() {
+        final String val = "abcdefg";
+        ConstantField<String> constString = new ConstantField<>(val);
+
+        constString.stream().limit(5).forEach(v -> Assert.assertEquals(val, v));
+    }
+}
