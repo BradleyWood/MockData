a00341d555b2a7d8730c4e9ce62cd871dab4a7d8
BradleyWood <bradley.wood@uoit.net>
2018-04-06 21:17:12 -0400

    make records streamable

--- src/main/java/org/mockdata/RecordEngine.java
+++ src/main/java/org/mockdata/RecordEngine.java
@@ -3,15 +3,16 @@ package org.mockdata;
 import org.jetbrains.annotations.NotNull;
 import org.mockdata.model.DataField;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
-public class RecordEngine {
+public class RecordEngine implements Iterable<Object[]> {
 
-    private final List<DataField> dataFields = new LinkedList<>();
+    private final List<DataField> dataFields = new ArrayList<>();
+    private int modCount = 0;
 
-    public RecordEngine(DataField[] dataFields) {
+    public RecordEngine(DataField... dataFields) {
         this.dataFields.addAll(Arrays.asList(dataFields));
     }
 
@@ -25,10 +26,61 @@ public class RecordEngine {
     }
 
     public void addDataFields(DataField... dataFields) {
-        this.dataFields.addAll(Arrays.asList(dataFields));
+        addDataFields(Arrays.asList(dataFields));
+    }
+
+    public void addDataFields(List<DataField> fields) {
+        modCount += fields.size();
+        this.dataFields.addAll(fields);
     }
 
     public void removeDataFields(DataField... dataFields) {
-        this.dataFields.removeAll(Arrays.asList(dataFields));
+        removeDataFields(Arrays.asList(dataFields));
+    }
+
+    public void removeDataFields(List<DataField> fields) {
+        modCount += fields.size();
+        this.dataFields.removeAll(fields);
+    }
+
+    @Override
+    public Spliterator<Object[]> spliterator() {
+        return Spliterators.spliteratorUnknownSize(iterator(), 0);
+    }
+
+    public Stream<Object[]> stream() {
+        return StreamSupport.stream(spliterator(), false);
+    }
+
+    @NotNull
+    @Override
+    public Iterator<Object[]> iterator() {
+        return new RecordItr();
+    }
+
+    private class RecordItr implements Iterator<Object[]> {
+
+        private final int expectedModCount;
+
+        RecordItr() {
+            this.expectedModCount = modCount;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return true;
+        }
+
+        @Override
+        public Object[] next() {
+            if (expectedModCount != modCount)
+                throw new ConcurrentModificationException("Record model modified while iterating");
+
+            Object[] data = new Object[dataFields.size()];
+            for (int i = 0; i < data.length; i++) {
+                data[i] = dataFields.get(i).generate();
+            }
+            return data;
+        }
     }
 }
