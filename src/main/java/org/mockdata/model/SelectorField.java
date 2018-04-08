c43433541e0f57d37cf057bd77e197a98f102de6
BradleyWood <bradley.wood@uoit.net>
2018-04-08 01:02:39 -0400

    select random fields from a set

---
+++ src/main/java/org/mockdata/model/SelectorField.java
@@ -0,0 +1,38 @@
+package org.mockdata.model;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
+/**
+ * Selects a random field from the set
+ *
+ * @param <T> Optional type parameter
+ */
+public class SelectorField<T> extends DataField<T> {
+
+    private final Random random = new Random();
+
+    private List<T> set;
+
+    public SelectorField(final T[] set) {
+        this(Arrays.asList(set));
+    }
+
+    public SelectorField(final List<T> set) {
+        if (set.isEmpty())
+            throw new IllegalArgumentException("Cannot select field from empty set");
+        this.set = set;
+    }
+
+    @NotNull
+    @Override
+    public T generate() {
+        return set.get(random.nextInt(set.size()));
+    }
+
+    @Override
+    public boolean isValid(Object element) {
+        return set.contains(element);
+    }
+}
