24e9639ce00469e9adeb7e55d458fbebb0be982a
BradleyWood <bradley.wood@uoit.net>
2018-04-13 16:07:10 -0400

    test data dependency

--- src/test/java/org/mockdata/fields/DependentFieldTest.java
+++ src/test/java/org/mockdata/fields/DependentFieldTest.java
@@ -2,6 +2,8 @@ package org.mockdata.fields;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockdata.Record;
+import org.mockdata.RecordEngine;
 import org.mockdata.testfields.IntDependentField;
 
 import java.util.Collections;
@@ -18,5 +20,19 @@ public class DependentFieldTest {
         Assert.assertEquals("null", em.generate());
 
         Assert.assertEquals(value.toString(), em.generate(data));
+    }
+
+    @Test
+    public void testDependentRows() {
+        RecordEngine re = new RecordEngine(new IntField(), new IntDependentField());
+
+        re.stream().limit(10).map(Record::getValues).forEach(data -> Assert.assertEquals(data[0].toString(), data[1]));
+    }
+
+    @Test
+    public void testMissingDependentData() {
+        RecordEngine re = new RecordEngine(new IntDependentField(), new IntField());
+        // test no data passage when dependent fields are placed first
+        re.stream().limit(10).map(Record::getValues).forEach(data -> Assert.assertEquals("null", data[0]));
     }
 }
