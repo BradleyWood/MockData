18f0128559844c236f938b478fe1a92117335af9
BradleyWood <bradley.wood@uoit.net>
2018-04-13 23:16:56 -0400

    a selector that excludes previously selected data in the same row

---
+++ src/main/java/org/mockdata/fields/ExclusiveSelectorField.java
@@ -0,0 +1,46 @@
+package org.mockdata.fields;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ExclusiveSelectorField<T> extends DataField<T> {
+
+    @NotNull
+    private final List<T> originalSet;
+
+    @Nullable
+    @DependentField(dependentOn = ExclusiveSelectorField.class)
+    private List<T> exclude = null;
+
+    public ExclusiveSelectorField(final T[] set) {
+        this(Arrays.asList(set));
+    }
+
+    public ExclusiveSelectorField(final List<T> set) {
+        this.originalSet = new ArrayList<>(set);
+    }
+
+    @NotNull
+    @Override
+    public T generate() {
+        final List<T> data = new LinkedList<>(originalSet);
+
+        if (exclude != null)
+            data.removeAll(exclude);
+
+        return data.get(random.nextInt(data.size()));
+    }
+
+    @Override
+    public boolean isValid(Object element) {
+        if (exclude != null && exclude.contains(element))
+            return false;
+
+        return originalSet.contains(element);
+    }
+}
