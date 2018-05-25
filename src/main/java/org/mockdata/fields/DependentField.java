452b8523c91c695f708b42a4f2f980161bc2e906
BradleyWood <bradley.wood@uoit.net>
2018-05-25 08:38:08 -0400

    dependent fields can only depend on DataFields

--- src/main/java/org/mockdata/fields/DependentField.java
+++ src/main/java/org/mockdata/fields/DependentField.java
@@ -6,6 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DependentField {
 
-    Class<?> dependentOn();
+    Class<? extends DataField> dependentOn();
 
 }
